111883214b35f22be7ad00dd0577e6565101f14c
KarthikGR <54638049+AlanWalker987@users.noreply.github.com>
2023-09-09 12:13:37 +0530

    Add files via upload

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function FinishScreen({ points, maxPossiblePoints, dispatch }) {
+
+    const percentage = (points / maxPossiblePoints) * 100;
+    return (
+        <>
+            <p className='result'>
+                You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
+            </p>
+            <button className='btn btn-ui' onClick={() => dispatch({ type: 'restart' })}>Restart Quiz</button>
+        </>
+    )
+}
---
+++ src/components/NextButton.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function NextButton({ dispatch, answer, index, numOfQuestions }) {
+
+    if (index < numOfQuestions - 1)
+        return (
+            <div>
+                <button className='btn btn-ui' onClick={() => dispatch({ type: 'nextQuestion' })}>Next</button>
+            </div>
+        )
+
+    if (index === numOfQuestions - 1)
+        return (
+            <div>
+                <button className='btn btn-ui' onClick={() => dispatch({ type: 'finish' })}>Finish</button>
+            </div>
+        )
+}
